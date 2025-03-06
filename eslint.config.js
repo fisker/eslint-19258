0144b0003f55dddf1a8a8651f862ded1b054d27d
fisker <lionkay@gmail.com>
2025-03-07 00:24:46 +0800

    Simplify reproduction

--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,19 @@
 import prettier from 'eslint-plugin-prettier'
-import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-})
 
 export default [
-  ...compat.extends(
-    'prettier',
-  ),
+  {
+    rules: {
+      'no-trailing-spaces': 'off',
+    }
+  },
   {
     plugins: {
       prettier,
     },
-
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.mocha,
-        NodeJS: 'readable',
-        Mocha: 'readable'
-      },
-
       parser: tsParser,
-      ecmaVersion: 2022,
-      sourceType: 'module',
-
-      parserOptions: {
-        project: 'tsconfig.json',
-        warnOnUnsupportedTypeScriptVersion: false
-      }
     },
-
-    settings: {
-      'import/extensions': ['.js', '.ts', '.tsx'],
-
-      'import/parsers': {
-        '@typescript-eslint/parser': ['.ts', '.tsx']
-      },
-
-      'import/resolver': {
-        node: {
-          extensions: ['.js', '.ts', '.tsx']
-        }
-      }
-    }
-  },
-  {
     files: ['sdk.gen.ts'],
     rules: {
       'prettier/prettier': 'error',
